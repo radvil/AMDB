68b08e3d581f4542897a17d5285a2c8b4eeae99d
Radvil <radvil@@access-mobile.com>
2024-03-07 23:43:12 +0800

    feat(app-shell): Add scroll top/down top nav effect

--- src/app/app-shell/app-shell.cmp.ts
+++ src/app/app-shell/app-shell.cmp.ts
@@ -1,7 +1,15 @@
-import { NgClass, NgTemplateOutlet } from '@angular/common';
+import {
+  DOCUMENT,
+  NgClass,
+  NgTemplateOutlet,
+  isPlatformBrowser,
+} from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  DestroyRef,
+  PLATFORM_ID,
+  ViewEncapsulation,
   inject,
   signal,
 } from '@angular/core';
@@ -11,13 +19,19 @@ import { ThemeService, UiRipple } from '@ui';
 import { MenuItem, menuItems } from './menu-items';
 import { SearchDialogCmp } from './search-dialog/search-dialog.cmp';
 import { SidebarCmp } from './sidebar/sidebar.cmp';
+import { debounceTime, distinctUntilChanged, fromEvent } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   standalone: true,
   selector: 'app-shell',
   styleUrl: 'app-shell.cmp.scss',
   templateUrl: 'app-shell.cmp.html',
-  host: { class: 'block min-h-[100svh]' },
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'block min-h-[100svh]',
+    '[attr.data-scroll]': 'scrollData()',
+  },
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     NgClass,
@@ -31,9 +45,45 @@ import { SidebarCmp } from './sidebar/sidebar.cmp';
   ],
 })
 export class AppShellCmp {
+  #doc = inject(DOCUMENT);
+  #platformId = inject(PLATFORM_ID);
+  #destroyRef = inject(DestroyRef);
   readonly themeService = inject(ThemeService);
   readonly menuItems = signal<MenuItem[]>(menuItems);
   readonly year = new Date().getFullYear();
   searchOpened = false;
   sidebarOpened = false;
+
+  readonly scrollData = signal<'' | 'up' | 'down'>('');
+
+  get isStuckedTop(): boolean {
+    return !(
+      this.#doc.body.scrollTop > 20 || this.#doc.documentElement.scrollTop > 20
+    );
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.#platformId)) {
+      let lastScrollY = 0;
+      fromEvent(this.#doc, 'scroll')
+        .pipe(
+          debounceTime(100),
+          distinctUntilChanged(),
+          takeUntilDestroyed(this.#destroyRef),
+        )
+        .subscribe(() => {
+          if (this.isStuckedTop) {
+            this.scrollData.set('');
+          } else {
+            const scrollData = this.scrollData();
+            if (window.scrollY > lastScrollY && scrollData !== 'down') {
+              this.scrollData.set('down');
+            } else if (window.scrollY < lastScrollY && scrollData === 'down') {
+              this.scrollData.set('up');
+            }
+            lastScrollY = window.scrollY;
+          }
+        });
+    }
+  }
 }
